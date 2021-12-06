b810dc7e08f2fd1bdca164c9fe10d5c4961e6852
thumbzzero <thumbzzero@gmail.com>
2021-12-06 19:50:43 +0900

    add fs.c that starts game screen

--- player1.c
+++ player1.c
@@ -1,1 +1,2 @@
 //player1(client1)을 위한 파일
+//client2 = player1
--- server.c
+++ server.c
@@ -1,1 +1,9 @@
 //서버를 위한 코드 작성 파일
+//server = player2
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+#include <socket.h>
+
